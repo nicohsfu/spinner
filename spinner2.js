675b356a95b9389c16a3105a060472799f8bc2c8
nicohsfu <marcnicohernandez@gmail.com>
2022-09-27 13:02:47 -0700

    refactor spinning animation

---
+++ spinner2.js
@@ -0,0 +1,22 @@
+const arr = ['|', '/', '-', '\\', "|"]; // had to escape the backslash - \ to \\
+
+let delay = 0;
+let count = 0;
+
+for (const char of arr) {
+  setTimeout(() => {
+    process.stdout.write(`\r${char}`);
+  }, delay += 200);
+
+  count++;
+}
+
+// if we want it to keep cycling
+// while (count <= 2) {
+//   for (const char of arr) {
+//     setTimeout(() => {
+//       process.stdout.write(`\r${char}`);
+//     }, delay += 200);
+//   }
+//   count++;
+// }
